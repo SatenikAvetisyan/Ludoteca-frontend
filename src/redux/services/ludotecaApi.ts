51ed83ab0d0695047f60139f76231ef35c19affc
Avetisyan <satenik.avetisyan@capgemini.com>
2025-06-19 21:41:13 +0200

    feat: listado filtrado creado para game

--- src/redux/services/ludotecaApi.ts
+++ src/redux/services/ludotecaApi.ts
@@ -1,6 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import type { Category } from "../../types/Category";
-import type { Author, AuthorResponse } from "../../types/Author";
+import type{ Game } from "../../types/Game";
+import type{ Category } from "../../types/Category";
+import type{ Author, AuthorResponse } from "../../types/Author";
 
 export const ludotecaAPI = createApi({
   reducerPath: "ludotecaApi",
@@ -40,64 +41,96 @@ export const ludotecaAPI = createApi({
       invalidatesTags: ["Category"],
     }),
     getAllAuthors: builder.query<Author[], null>({
-        query: () => "author",
-        providesTags: ["Author" ],
-      }),
-      getAuthors: builder.query<
-        AuthorResponse,
-        { pageNumber: number; pageSize: number }
-      >({
-        query: ({ pageNumber, pageSize }) => {
-          return {
-            url: "author",
-            method: "POST",
-            body: {
-              pageable: {
-                pageNumber,
-                pageSize,
-              },
+      query: () => "author",
+      providesTags: ["Author"],
+    }),
+    getAuthors: builder.query<
+      AuthorResponse,
+      { pageNumber: number; pageSize: number }
+    >({
+      query: ({ pageNumber, pageSize }) => {
+        return {
+          url: "author",
+          method: "POST",
+          body: {
+            pageable: {
+              pageNumber,
+              pageSize,
             },
-          };
-        },
-        providesTags: ["Author"],
-      }),
-      createAuthor: builder.mutation({
-        query: (payload) => ({
-          url: "/author",
-          method: "PUT",
-          body: payload,
-          headers: {
-            "Content-type": "application/json; charset=UTF-8",
           },
-        }),
-        invalidatesTags: ["Author"],
+        };
+      },
+      providesTags: ["Author"],
+    }),
+    createAuthor: builder.mutation({
+      query: (payload) => ({
+        url: "/author",
+        method: "PUT",
+        body: payload,
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
       }),
-      deleteAuthor: builder.mutation({
-        query: (id: string) => ({
-          url: `/author/${id}`,
-          method: "DELETE",
-        }),
-        invalidatesTags: ["Author"],
+      invalidatesTags: ["Author"],
+    }),
+    deleteAuthor: builder.mutation({
+      query: (id: string) => ({
+        url: `/author/${id}`,
+        method: "DELETE",
       }),
-      updateAuthor: builder.mutation({
-        query: (payload: Author) => ({
-          url: `author/${payload.id}`,
-          method: "PUT",
-          body: payload,
-        }),
-        invalidatesTags: ["Author", "Game"],
+      invalidatesTags: ["Author"],
+    }),
+    updateAuthor: builder.mutation({
+      query: (payload: Author) => ({
+        url: `author/${payload.id}`,
+        method: "PUT",
+        body: payload,
       }),
+      invalidatesTags: ["Author", "Game"],
+    }),
+    getGames: builder.query<Game[], { title: string; idCategory: string }>({
+      query: ({ title, idCategory }) => {
+        return {
+          url: "game/",
+          params: { title, idCategory },
+        };
+      },
+      providesTags: ["Game"],
+    }),
+    createGame: builder.mutation({
+      query: (payload: Game) => ({
+        url: "/game",
+        method: "PUT",
+        body: { ...payload },
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      }),
+      invalidatesTags: ["Game"],
+    }),
+    updateGame: builder.mutation({
+      query: (payload: Game) => ({
+        url: `game/${payload.id}`,
+        method: "PUT",
+        body: { ...payload },
+      }),
+      invalidatesTags: ["Game"],
+    }),
+
   }),
 });
 
 export const {
-    useGetCategoriesQuery,
-    useCreateCategoryMutation,
-    useDeleteCategoryMutation,
-    useUpdateCategoryMutation,
-    useCreateAuthorMutation,
-    useDeleteAuthorMutation,
-    useGetAllAuthorsQuery,
-    useGetAuthorsQuery,
-    useUpdateAuthorMutation,
+  useGetCategoriesQuery,
+  useCreateCategoryMutation,
+  useDeleteCategoryMutation,
+  useUpdateCategoryMutation,
+  useCreateAuthorMutation,
+  useDeleteAuthorMutation,
+  useGetAllAuthorsQuery,
+  useGetAuthorsQuery,
+  useUpdateAuthorMutation,
+  useCreateGameMutation,
+  useGetGamesQuery,
+  useUpdateGameMutation
 } = ludotecaAPI;
