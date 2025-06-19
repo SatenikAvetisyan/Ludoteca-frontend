d36dcd8f45ceb42fb6fa75c5e8433f92cb4a168a
Avetisyan <satenik.avetisyan@capgemini.com>
2025-06-19 19:41:10 +0200

    feat: listado simple creado para categorias

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Game } from "./pages/Game/Game";
+import { Author } from "./pages/Author/Author";
+import { Category } from "./pages/Category/Category";
+import { Layout } from "./components/Layout";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+import { LoaderProvider } from "./context/LoaderProvider";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <LoaderProvider>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route index path="games" element={<Game />} />
+            <Route path="categories" element={<Category />} />
+            <Route path="authors" element={<Author />} />
+            <Route path="*" element={<Navigate to="/games" />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  </LoaderProvider>
+  );
 }
 
-export default App
+export default App;
